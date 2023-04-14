b29f82cd034c0cd00df29af462738bc9b61032a7
David <moncho021509@gmail.com>
2023-04-13 23:30:07 -0500

    cursoTerminado

--- Curso/Java/uberX.java
+++ Curso/Java/uberX.java
@@ -4,9 +4,16 @@ public class uberX extends Car{
   String brand;
   String model;
   
-  public uberX(String license, Account driver, String brand, String model){
-    super(license, driver);
+  public uberX(String license, Account driver, String brand, String model, int passenger){
+    super(license, driver,passenger);
     this.brand = brand;
     this.model = model;
   }
+  
+  @Override
+  void printDataCar() {
+    super.printDataCar();
+    System.out.println("Model: " + model + " Brand: " + brand);
+  }
+  
 }
